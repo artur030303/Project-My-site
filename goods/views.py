912d0bb98ee4c87eeb56a126fd3b8012e714925a
Artur <aryzkevic@gmail.com>
2025-01-02 16:45:49 +0100

    Pagination

--- goods/views.py
+++ goods/views.py
@@ -1,4 +1,5 @@
 from turtle import title
+from unicodedata import category
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -7,16 +8,39 @@ from goods.models import Products
 
 
 # Create your views here.
-def catalog(request) -> HttpResponse:
+def catalog(request, category_slug) -> HttpResponse:
 
-    goods = Products.objects.all()
+    page_number = request.GET.get("page")
+    on_sale = request.GET.get("on_sale", None)
+    order_by = request.GET.get("order_by", None)
+
+    if category_slug == "all":
+        goods = Products.objects.all()
+    else:
+        goods = Products.objects.filter(category__slug=category_slug)
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != "default":
+        goods = goods.order_by(order_by)
 
     paginator = Paginator(goods, 3)
-    page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
-    return render(request, "goods/catalog.html", {"page_obj": page_obj})
+    context = {
+        "page_obj": page_obj,
+        "goods": goods,
+        "on_sale": on_sale,
+        "order_by": order_by,
+        "slug_url": category_slug,
+    }
+
+    return render(request, "goods/catalog.html", context)
 
 
-def product(request):
-    return render(request, "goods/product.html")
+def product(request, product_slug):
+    product = Products.objects.get(slug=product_slug)
+
+    context = {"product": product}
+    return render(request, "goods/product.html", context=context)
